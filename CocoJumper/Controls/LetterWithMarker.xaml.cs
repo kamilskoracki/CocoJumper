4ad859dfb4a77ec60da4c4fb698cf4f60a612476
Michał Krawczyk <edii.poz@gmail.com>
2019-03-26 11:59:11 +0100

    static design of letter with marker control

--- CocoJumper/Controls/LetterWithMarker.xaml.cs
+++ CocoJumper/Controls/LetterWithMarker.xaml.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace CocoJumper.Controls
+﻿namespace CocoJumper.Controls
 {
     /// <summary>
     /// Interaction logic for LetterWithMarker.xaml
     /// </summary>
-    public partial class LetterWithMarker : UserControl
+    public partial class LetterWithMarker
     {
         public LetterWithMarker(string text)
         {
             InitializeComponent();
-            this.Content = text;
+            Content = text;
+        }
+
+        private void TextBox_TextChanged()
+        {
+
         }
     }
 }
